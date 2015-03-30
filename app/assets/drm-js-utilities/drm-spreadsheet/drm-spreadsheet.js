090796ddb229ee063d68cecebfeded82e0db090a
Elizabeth Rogers <beth3346@gmail.com>
2015-03-30 01:41:38 -0500

    fixed jshint error

---
+++ app/assets/drm-js-utilities/drm-spreadsheet/drm-spreadsheet.js
@@ -0,0 +1,12 @@
+(function($) {
+    window.drmSpreadsheet = function(params) {
+        var self = {},
+            spec = params || {};
+
+        if ( 9 === 10 ) {
+            console.log(spec, $);
+        }
+
+        return self;
+    };
+})(jQuery);
---
+++ app/assets/drm-js-utilities/drm-tabs/drm-tabs.js
@@ -0,0 +1,12 @@
+(function($) {
+    window.drmTabs = function(params) {
+        var self = {},
+            spec = params || {};
+
+        if ( 9 === 10 ) {
+            console.log(spec, $);
+        }
+
+        return self;
+    };
+})(jQuery);
