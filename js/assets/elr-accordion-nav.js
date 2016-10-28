1c367bf9c2ef9678ed3165d7ec1f3ac502bddc27
Elizabeth Rogers <beth3346@gmail.com>
2016-10-28 08:05:15 -0500

    create function to get the current page to DRY out code

--- js/assets/elr-accordion-nav.js
+++ js/assets/elr-accordion-nav.js
@@ -46,46 +46,42 @@ const elrAccordionNav = function(params) {
     // remove the hash mark from a url hash
     const trimHash = function(hash) {
         return hash.slice(0,1);
-    }
+    };
+
+    const getCurrentPage = function(location, hash) {
+        const startIndex = location.lastIndexOf('/') + 1;
+
+        if (hash) {
+            return location.slice(startIndex) + hash;
+        } else if (location.slice(0,1) === '/') {
+            return location.slice(startIndex);
+        }
+
+        return location;
+    };
 
     const getCurrent = function() {
         const location = window.location.pathname;
         const hash = window.location.hash;
-        const startIndex = location.lastIndexOf('/') + 1;
-        let currentPage;
-        let $target;
-        let $currentList;
-
-        if (hash) {
-            currentPage = location.slice(startIndex) + hash;
-        } else if (location.slice(0,1) === '/') {
-            currentPage = location.slice(startIndex);
-        } else {
-            currentPage = location;
-        }
-
-        $target = $container.find(`a[href="${currentPage}"]`).addClass('active');
+        const currentPage = getCurrentPage(location, hash);
+        const $target = $container.find(`a[href="${currentPage}"]`).addClass('active');
 
         if ($target.length) {
             return $target.closest('ul').parent('li');
         } else if (hash) {
-            // if the hash is not in the menu remove it and return the url
-            currentPage = location.slice(startIndex);
-            $target = $container.find(`a[href="${currentPage}"]`).addClass('active');
-
-            return $target.closest('ul').parent('li');
+            // if the hash doesn't exist in the menu remove it and use the url instead
+            return $container.find(`a[href="${location.slice(location.lastIndexOf('/') + 1)}"]`)
+                .addClass('active')
+                .closest('ul')
+                .parent('li');
         } else {
             return false;
         }
     };
 
     const showCurrent = function($currentList) {
-        // if ($currentList) {
-            $currentList.find('ul').show();
-            $currentList.find(`.${iconClass}`).removeClass(expandIconClass).addClass(collapseIconClass);
-        // }
-
-        // return;
+        $currentList.find('ul').show();
+        $currentList.find(`.${iconClass}`).removeClass(expandIconClass).addClass(collapseIconClass);
     };
 
     if ($container.length) {
@@ -95,7 +91,7 @@ const elrAccordionNav = function(params) {
             .children('a');
         const $content = $label.next('ul');
         const $expandedContent = $container.find(`.${contentHolderClass}[data-state=expanded]`);
-        let $currentList = getCurrent();
+        const $currentList = getCurrent();
         const $icons = $label.find(`.${iconClass}`);
 
         if (!$currentList) {
@@ -110,10 +106,9 @@ const elrAccordionNav = function(params) {
             e.preventDefault();
 
             $container.find('a.active').removeClass('active');
-            $currentList = getCurrent();
             $content.hide();
             $icons.removeClass(collapseIconClass).addClass(expandIconClass);
-            showCurrent($currentList);
+            showCurrent(getCurrent());
         });
 
         $label.on('click', function(e) {
