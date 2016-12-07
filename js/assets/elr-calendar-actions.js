69c4de22b34ec123dd2786f56eef567f30bf3bf8
Elizabeth Rogers <beth3346@gmail.com>
2016-12-07 08:23:54 -0600

    refactor advance calendar functions

--- js/assets/elr-calendar-actions.js
+++ js/assets/elr-calendar-actions.js
@@ -21,6 +21,61 @@ const elrCalendarActions = function({
         'date': 'elr-date'
     };
 
+    const getPrevWeek = ($calendarInner, dateObj) => {
+        const lastYear = dateObj.year - 1;
+        const lastMonth = (dateObj.month === 0) ? 11 : dateObj.month - 1;
+        const firstDay = $calendarInner.find(`.${classes.date}`).first().data('date');
+        const lastDayOfPrevMonth = elrTime.getDaysInMonth({
+            'month': lastMonth,
+            'date': dateObj.date,
+            'year': dateObj.year
+        });
+
+        // if its the first week and the first day of the month is a sunday
+        if ((firstDay === 1) && (view === 'week')) {
+            dateObj.date = lastDayOfPrevMonth;
+            dateObj.year = (dateObj.month === 0) ? lastYear : dateObj.year;
+            dateObj.month = lastMonth;
+        } else if ((firstDay < 7) && (view === 'week')) {
+            dateObj.date = firstDay - 1;
+        } else if ((firstDay === 1) && (view === 'date')) {
+            dateObj.date = lastDayOfPrevMonth - 6 ; // 30 - 6 1 24
+            dateObj.year = (dateObj.month === 0) ? lastYear : dateObj.year;
+            dateObj.month = lastMonth;
+        } else if ((firstDay < 7) && (view === 'date')) {
+            dateObj.date = lastDayOfPrevMonth - (7 - firstDay); // 31 - (7 - 3) = 27 27  28 - (7 - 6) = 27 6 27
+            dateObj.year = (dateObj.month === 0) ? lastYear : dateObj.year;
+            dateObj.month = lastMonth;
+        } else {
+            dateObj.date = firstDay - 7;
+        }
+
+        return dateObj;
+    };
+
+    const getNextWeek = ($calendarInner, dateObj) => {
+        const lastDay = $calendarInner.find(`.${classes.date}`).last().data('date');
+        const lastDayOfMonth = elrTime.getDaysInMonth(dateObj);
+        const nextYear = dateObj.year + 1;
+        const nextMonth = (dateObj.month === 11) ? 0 : dateObj.month + 1;
+
+        if ((lastDay === lastDayOfMonth) && (view === 'week')) {
+            dateObj.date = 1;
+            dateObj.year = (dateObj.month === 11) ? nextYear : dateObj.year;
+            dateObj.month = nextMonth;
+        } else if ((lastDay + 7 > lastDayOfMonth) && (view === 'week')) {
+            dateObj.date = lastDayOfMonth;
+        } else if ((lastDay + 7 > lastDayOfMonth) && (view === 'date')) {
+            dateObj.date = 7 - (lastDayOfMonth - lastDay); // 31 - 29 = 2 ? 7 - 2 = 5
+            dateObj.year = (dateObj.month === 11) ? nextYear : dateObj.year;
+            dateObj.month = nextMonth;
+        } else {
+            dateObj.date = lastDay + 7;
+        }
+
+        return dateObj;
+    };
+
     const self = {
         advanceYear: (dir, evts, $cal) => {
             const $calendarInner = $cal.find(`.${calendarInnerClass}`);
@@ -49,64 +104,23 @@ const elrCalendarActions = function({
             const $calendarInner = $cal.find(`.${calendarInnerClass}`);
             const month = $calendarInner.data('month');
             const year = $calendarInner.data('year');
-            const dateObj = {
-                'month': month,
-                'date': 1,
-                'year': year
-            };
-            const nextYear = dateObj.year + 1;
-            const lastYear = dateObj.year - 1;
-            const nextMonth = (dateObj.month === 11) ? 0 : dateObj.month + 1;
-            const lastMonth = (dateObj.month === 0) ? 11 : dateObj.month - 1;
-            const view = 'week';
-            let firstDay;
-            let lastDayOfPrevMonth;
-            let lastDay;
-            let lastDayOfMonth;
+            let dateObj;
 
             if (dir === 'prev') {
-                firstDay = $calendarInner.find(`.${classes.date}`).first().data('date');
-                lastDayOfPrevMonth = elrTime.getDaysInMonth({
-                    'month': lastMonth,
-                    'date': dateObj.date,
-                    'year': dateObj.year
+                dateObj = getPrevWeek($calendarInner, {
+                    'month': month,
+                    'date': 1,
+                    'year': year
                 });
-
-                if ((firstDay === 1) && (view === 'week')) {
-                    dateObj.date = lastDayOfPrevMonth;
-                    dateObj.year = (dateObj.month === 0) ? lastYear : dateObj.year;
-                    dateObj.month = lastMonth;
-                } else if ((firstDay < 7) && (view === 'week')) {
-                    dateObj.date = firstDay - 1;
-                } else if ((firstDay === 1) && (view === 'date')) {
-                    dateObj.date = lastDayOfPrevMonth - 6 ; // 30 - 6 1 24
-                    dateObj.year = (dateObj.month === 0) ? lastYear : dateObj.year;
-                    dateObj.month = lastMonth;
-                } else if ((firstDay < 7) && (view === 'date')) {
-                    dateObj.date = lastDayOfPrevMonth - (7 - firstDay); // 31 - (7 - 3) = 27 27  28 - (7 - 6) = 27 6 27
-                    dateObj.year = (dateObj.month === 0) ? lastYear : dateObj.year;
-                    dateObj.month = lastMonth;
-                } else {
-                    dateObj.date = firstDay - 7;
-                }
-            } else if (dir === 'next') {
-                lastDay = $calendarInner.find(`.${classes.date}`).last().data('date');
-                lastDayOfMonth = elrTime.getDaysInMonth(dateObj);
-
-                if ((lastDay === lastDayOfMonth) && (view === 'week')) {
-                    dateObj.date = 1;
-                    dateObj.year = (dateObj.month === 11) ? nextYear : dateObj.year;
-                    dateObj.month = nextMonth;
-                } else if ((lastDay + 7 > lastDayOfMonth) && (view === 'week')) {
-                    dateObj.date = lastDayOfMonth;
-                } else if ((lastDay + 7 > lastDayOfMonth) && (view === 'date')) {
-                    dateObj.date = 7 - (lastDayOfMonth - lastDay); // 31 - 29 = 2 ? 7 - 2 = 5
-                    dateObj.year = (dateObj.month === 11) ? nextYear : dateObj.year;
-                    dateObj.month = nextMonth;
-                } else {
-                    dateObj.date = lastDay + 7;
-                }
+            } else {
+                dateObj = getNextWeek($calendarInner, {
+                    'month': month,
+                    'date': 1,
+                    'year': year
+                });
             }
+
+            elrCreate.changeCalendar(view, dateObj, $cal, evts);
         },
         advanceMonth: (dir, evts, $cal) => {
             const $calendarInner = $cal.find(`.${calendarInnerClass}`);
